045e3d47d5afbc967b3eaf7f051131aa2e53704f
YuanSuyi <yuansuyi@hzdusun.com>
2019-06-27 15:52:48 +0800

    pyone: update install.sh

--- pyone/install.sh
+++ pyone/install.sh
@@ -5,9 +5,11 @@ git clone https://github.com/abbeyokgo/PyOne.git
 cd PyOne
 cp self_config.py.sample self_config.py
 pip2 install -r requirements.txt
-wget -N --no-check-certificate https://www.moerats.com/usr/shell/PyOne/pyone.service -P '/etc/systemd/system/'
 
+wget -N --no-check-certificate https://raw.githubusercontent.com/ysy/ss/master/pyone/pyone.service -P '/etc/systemd/system/'
+
+wget -N --no-check-certificate https://raw.githubusercontent.com/ysy/ss/master/pyone/pyone.sh -P '/root/'
+chmod +x /root/pyone.sh
 
 systemctl start redis-server mongdb pyone
 systemctl enable redis-server mongdb pyone
-
